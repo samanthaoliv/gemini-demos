832c120e39ff750f01853397a94e262f15b0dbf2
samanthaoliv <162748473+samanthaoliv@users.noreply.github.com>
2024-09-09 20:31:07 -0300

    ds

--- gemini/sample-apps/gemini-streamlit-cloudrun/my_app.py
+++ gemini/sample-apps/gemini-streamlit-cloudrun/my_app.py
@@ -247,7 +247,7 @@ with tab2:
                         # response = get_gemini_response(selected_model, [prompt, vide_desc_img]) 
 
                         # Se get_gemini_response espera a URI do vídeo:
-                        response = get_gemini_response(selected_model, [prompt, vide_desc_uri]) 
+                        response = get_gemini_response(selected_model, [prompt, video_bytes]) 
 
                         st.markdown(response)
                         st.markdown("\n\n\n")
